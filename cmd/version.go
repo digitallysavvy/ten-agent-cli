33db74e91ac39233220591169143e78cecdbc339
digitallysavvy <hermes.frangoudis@gmail.com>
2024-09-23 22:38:06 -0400

    version based on release

--- cmd/version.go
+++ cmd/version.go
@@ -6,14 +6,18 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "0.0.11" // TODO: use build flags to set version
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of TEN Agent CLI",
 	Long:  `All software has versions. This is TEN Agent's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("TEN Agent CLI v%s\n", version)
+		fmt.Printf("TEN Agent CLI v%s (commit: %s, built at: %s)\n", version, commit, date)
 	},
 }
 
